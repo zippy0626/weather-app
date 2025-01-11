2031f9a72023175e037a150e21daf9f086e0b9c4
zippy0626 <zippy0626@gmail.com>
2025-01-11 12:47:55 -0500

    add setup files

---
+++ webpack.dev.js
@@ -0,0 +1,11 @@
+//used Webpack Production config setup
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+});
---
+++ webpack.prod.js
@@ -0,0 +1,8 @@
+//used Webpack Production config setup
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+});
